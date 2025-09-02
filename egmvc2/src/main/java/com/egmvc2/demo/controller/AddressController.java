c562c3755edc73b183f93ab6ba8cca6372c80c03
snl <1978719130@qq.com>
2025-09-02 22:21:56 +0800

    为addressController.java添加文档注释

--- egmvc2/src/main/java/com/egmvc2/demo/controller/AddressController.java
+++ egmvc2/src/main/java/com/egmvc2/demo/controller/AddressController.java
@@ -38,22 +38,40 @@ public class AddressController {
         return "list";
     }
 
+    /**
+     * 删除信息的确认视图，只接受GET请求
+     * @return {@code confirm}视图模板
+     */
     @GetMapping("/adress/delete/confirm")
     public String delete_confirm() {
         return "confirm";
     }
 
+    /**
+     * 删除地址信息后的重定向函数，只接受POST请求
+     * @param id 地址id，用来标识地址信息
+     * @return {@code delete_complete}视图模板
+     */
     @PostMapping("/adress/delete/complete")
     public String deleteComplete(Integer id) {
         System.out.println("删除成功");
         return "delete_complete";
     }
 
+    /**
+     * 更新确认视图函数，只接受GET请求
+     * @return {@code update_confirm}视图模板
+     */
     @GetMapping("/adress/update/confirm")
     public String updateConfirm() {
         return "update_confirm";
     }
 
+    /**
+     * 更新试图后重定向视图，只接受POST请求
+     * @param adressInfoVO1 {@code addressInfo}的视图类，用来封装部分属性
+     * @return {@code update_complete}视图模板
+     */
     @PostMapping("/adress/update/complete")
     public String updateComplete(adressInfoVO1 adressInfoVO1) {
         System.out.println(adressInfoVO1);
