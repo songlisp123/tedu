4a9d0a09bb0923aa755fc990bc789de7e6900718
snl <1978719130@qq.com>
2025-09-02 22:01:13 +0800

    创建adressInfo的视图类

---
+++ egmvc2/src/main/java/com/egmvc2/demo/pojo/vo/adressInfoVO1.java
@@ -0,0 +1,70 @@
+package com.egmvc2.demo.pojo.vo;
+
+public class adressInfoVO1 {
+    private Integer id;
+    private String receiver;
+    private  String address;
+    private String email;
+    private String mobile;
+    private String tag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    @Override
+    public String toString() {
+        return "adressInfoVO1{" +
+                "id=" + id +
+                ", receiver='" + receiver + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", mobile='" + mobile + '\'' +
+                ", tag='" + tag + '\'' +
+                '}';
+    }
+}
