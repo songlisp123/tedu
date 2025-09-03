180b43bd743506e99e4fba90d6b616dd427f03bc
snl <1978719130@qq.com>
2025-09-03 18:40:44 +0800

    创建新工程notice

---
+++ notice/src/main/java/com/notice/demo/Notice02Application.java
@@ -0,0 +1,13 @@
+package com.notice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Notice02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Notice02Application.class, args);
+	}
+
+}
---
+++ notice/src/test/java/com/notice/demo/Notice02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notice.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Notice02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
