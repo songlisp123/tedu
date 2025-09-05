3ef8a9707a7fd57dcc9226dd4e1dbe46edfdffc0
snl <1978719130@qq.com>
2025-09-05 15:31:29 +0800

    使用lombok注解轻量化代码构造

--- notice/src/main/java/com/notice/demo/pojo/dto/NoticeListQuery.java
+++ notice/src/main/java/com/notice/demo/pojo/dto/NoticeListQuery.java
@@ -1,35 +1,14 @@
 package com.notice.demo.pojo.dto;
 
+import lombok.Data;
+
 /**
  * {@code notice}视图对象，用来从客户端接受以下数据：<br />
  * {@code title} <br />
  * {@code type}
  */
+@Data
 public class NoticeListQuery {
     private String title;
     private Integer type;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    @Override
-    public String toString() {
-        return "NoticeListQuery{" +
-                "title='" + title + '\'' +
-                ", type=" + type +
-                '}';
-    }
 }
--- notice/src/main/java/com/notice/demo/pojo/dto/noticeDto.java
+++ notice/src/main/java/com/notice/demo/pojo/dto/noticeDto.java
@@ -1,6 +1,9 @@
 package com.notice.demo.pojo.dto;
 
+import lombok.Data;
+
 import java.util.Date;
+
 
 /**
  * {@code notice}视图对象，用来从客户端接受以下数据：<br />
@@ -9,51 +12,10 @@ import java.util.Date;
  * {@code type} <br />
  * {@code status}
  */
+@Data
 public class noticeDto {
     private String title;
     private String content;
     private Integer type;
     private Integer status;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "noticeDto{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                ", type=" + type +
-                ", status=" + status +
-                '}';
-    }
 }
