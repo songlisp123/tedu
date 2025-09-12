7f36b3667e45201f0b5c1ad6b9e65761e67bb229
snl <1978719130@qq.com>
2025-09-12 16:54:42 +0800

    更新项目工程

---
+++ _09secutity/src/test/java/com/_09secutity/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._09secutity.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
